06e6a5b2bba0b78f5a63f30eb0ef6a99147cd629
diegowanborges <diegowanborges@gmail.com>
2021-01-27 20:56:54 -0300

    Home - Finish

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './styles.scss'
 import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg'
+import ButtonIcon from '../../core/components/ButtonIcon'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => (
     <>
@@ -9,6 +12,11 @@ const Home = () => (
                 <div className="col-6"  >
                 <h1 className="text-title">Conheça o melhor <br/> catálogo de produtos</h1>
                 <p className="text-subtitle">Ajudaremos você a encontrar os melhores <br/> produtos disponíveis no mercado.</p>
+                <Link to="/catalog">
+                    <ButtonIcon text="INICIE AGORA SUA BUSCA" />
+                </Link>
+                
+                
                 </div>
                 <div className="col-6">
                     <MainImage className="main-image" />
