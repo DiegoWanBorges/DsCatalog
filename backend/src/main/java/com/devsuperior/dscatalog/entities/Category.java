0eb808bd82638387b4df80d53b4cab732c97417c
diegowanborges <diegowanborges@gmail.com>
2021-02-10 10:27:39 -0300

    Add User and Role

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -22,6 +25,9 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant createdAt;
@@ -69,6 +75,12 @@ public class Category implements Serializable {
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt=Instant.now();
+	}
+
+	
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
--- front-web/src/core/components/Pagination/index.tsx
+++ front-web/src/core/components/Pagination/index.tsx
@@ -28,7 +28,7 @@ const Pagination = ( {totalPages, activePage, onChange} :Props ) => {
                         key={item}
                         className={`pagination-item ${item===activePage ? 'active' : ''}`}
                         onClick={() => onChange(item)}
-
+                        
                     >
                         {item+1}
                     </div>
