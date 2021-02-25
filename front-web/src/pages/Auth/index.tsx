028b6c10ce941cc5170a57183fe9fe5d04958bf2
diegowanborges <diegowanborges@gmail.com>
2021-02-25 11:09:08 -0300

    Route Auth - Part 2

--- front-web/src/pages/Auth/index.tsx
+++ front-web/src/pages/Auth/index.tsx
@@ -1,6 +1,7 @@
 import './styles.scss'
 
 import { ReactComponent as AuthImage } from 'core/assets/images/login.svg'
+import { Route, Switch } from 'react-router-dom';
 const Auth = () =>{
     return (
         <div className="auth-container">
@@ -16,7 +17,17 @@ const Auth = () =>{
 
             </div>
             <div className="auth-content">
-                <h1>Formulario de login</h1>
+            <Switch>
+                <Route path="/admin/auth/login">
+                    <h1>Login</h1>
+                </Route>
+                <Route path="/admin/auth/register">
+                    <h1>Register</h1>
+                </Route>
+                <Route path="/admin/auth/recover">
+                    <h1>Recover</h1>
+                </Route>                
+            </Switch>
             </div>
         </div>
     );
