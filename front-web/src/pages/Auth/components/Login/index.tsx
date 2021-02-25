f5c675f403991488c6770e5d18693784948a20de
diegowanborges <diegowanborges@gmail.com>
2021-02-25 13:24:42 -0300

    Form Login - Part 2

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,3 +1,5 @@
+import ButtonIcon from 'core/components/ButtonIcon';
+import { Link } from 'react-router-dom';
 import CardAuth from '../Card';
 import './styles.scss'
 
@@ -5,11 +7,40 @@ const Login = () => {
     return (
         <CardAuth title="LOGIN">
             <form className="login-form">
-                <h1>Form login</h1>
+                <input
+                    type="email"
+                    className="form-control imput-base margin-bottom-30"
+                    placeholder="Email"
+                />
+                <input
+                    type="password"
+                    className="form-control imput-base"
+                    placeholder="Senha"
+                />
+                <Link to="/admin/auth/recover" className="login-link-recover">
+                    Esqueci a senha?
+                </Link>
+                <div className="login-submit">
+                    <ButtonIcon
+                        text="LOGAR"
+                    />
+                </div>
+
+                <div className="text-center">
+                    <span className="login-not-register">
+                        Não tem Cadastro?
+                    </span>
+
+                    <Link to="/admin/auth/register" className="login-link-register">
+                        CADASTRAR
+                    </Link>
+                </div>
+
+
             </form>
         </CardAuth>
 
-        
+
     )
 }
 export default Login;
