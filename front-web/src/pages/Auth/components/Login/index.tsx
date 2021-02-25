e0473f7e67c4fff94590852775a0c38b83ead187
diegowanborges <diegowanborges@gmail.com>
2021-02-25 14:54:08 -0300

    Form Login - Finish

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -2,20 +2,34 @@ import ButtonIcon from 'core/components/ButtonIcon';
 import { Link } from 'react-router-dom';
 import CardAuth from '../Card';
 import './styles.scss'
+import { useForm } from 'react-hook-form';
+
+type FormData ={
+    email: string;
+    password: string;
+}
 
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>(); 
+    const onSubmit = (data: FormData) =>{
+        console.log(data);
+    }
     return (
         <CardAuth title="LOGIN">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
                     className="form-control imput-base margin-bottom-30"
                     placeholder="Email"
+                    name="email"
+                    ref={register}
                 />
                 <input
                     type="password"
                     className="form-control imput-base"
                     placeholder="Senha"
+                    name="password"
+                    ref={register}
                 />
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha?
