9a71396d682fccce4a84654dd20ade388f27db39
diegowanborges <diegowanborges@gmail.com>
2021-02-25 16:38:56 -0300

    Localstorage

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,26 +1,45 @@
 import ButtonIcon from 'core/components/ButtonIcon';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import CardAuth from '../Card';
 import './styles.scss'
 import { useForm } from 'react-hook-form';
 import { makeLogin } from 'core/utils/request';
+import { useState } from 'react';
+import { saveSessionData } from 'core/utils/auth';
 
-type FormData ={
+type FormData = {
     username: string;
     password: string;
 }
 
 const Login = () => {
-    const { register, handleSubmit } = useForm<FormData>(); 
-    const onSubmit = (data: FormData) =>{
-        console.log(data);
-        makeLogin(data);
+    const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
+    const history = useHistory();
+
+    const onSubmit = (data: FormData) => {
+        makeLogin(data)
+                  .then(response=> {
+                    setHasError(false);
+                    saveSessionData(response.data);
+                    history.push("/admin");
+                  })
+                  .catch(()=>{
+                      setHasError(true);
+                  })
+
+        ;
     }
-
-    
-
     return (
         <CardAuth title="LOGIN">
+
+            {
+                hasError && (
+                    <div className="alert alert-danger mt-5">
+                        Usuário ou senha inválido!
+                    </div>
+                )
+            }
             <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
