41c0c039bdde270fb848e6accf3e0d0703435389
diegowanborges <diegowanborges@gmail.com>
2021-03-10 19:31:19 -0300

    Add navigation

---
+++ mobile/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {View, Text} from 'react-native'
+
+const Home: React.FC = ()  =>{
+    return (
+        <View>
+            <Text>
+                Bem Vindo a Tela Home
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Home, Catalog } from '../pages'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
+
+const Routes: React.FC = () =>{
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Catalog" component={Catalog} />
+        </Stack.Navigator>
+    )
+}
+
+export default Routes;
